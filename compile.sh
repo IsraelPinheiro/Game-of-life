22f723fa4408e54b7b534b1d6d207ac41e07d1fb
Israel Reis Pinheiro <israel.pinheiro@live.com>
2021-05-30 21:31:24 -0300

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,1 @@
+pyinstaller --onefile --icon=glider.ico life.py
---
+++ life.py
@@ -0,0 +1,67 @@
+import pygame
+import os, sys
+from grid import Grid
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+
+size = WIDTH, HEIGHT = 1920,1080 #Screen size
+
+#Colors dfinition
+BLACK = (0,0,0)
+GRAY_25 = (63,63,63)
+GRAY_50 = (127,127,127)
+GRAY_75 = (191,191,191)
+WHITE = (255,255,255)
+BLUE = (0,14,71)
+
+pygame.init()
+pygame.display.set_caption("Conway's Game of Life")
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+
+scale = 15
+offset = 1
+
+run = True
+pause = False
+
+grid = Grid(width=WIDTH, height=HEIGHT, scale=scale, offset=offset)
+grid.random_2d_array()
+
+while run:
+    if pause == False:
+        clock.tick(fps)
+        screen.fill(BLACK)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_SPACE: #Space - Pause the game on the current state
+                pause = not pause
+            if event.key == pygame.K_f: #F - Set the game to a full array and pauses it
+                grid.full_2d_array()
+                pause = True
+            if event.key == pygame.K_c: #C - Set the game to a clean array and pauses it
+                grid.clean_2d_array()
+                pause = True
+            if event.key == pygame.K_r: #R - Set the game to a random state and unpauses it if paused
+                grid.random_2d_array()
+                pause = False
+            if event.key == pygame.K_x: #X - Quit
+                pygame.quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+            clicked_pos_x = int(mouse_pos_x/scale)
+            clicked_pos_y = int(mouse_pos_y/scale)
+            if grid.grid_array[clicked_pos_x][clicked_pos_y] == 1:
+                grid.grid_array[clicked_pos_x][clicked_pos_y] = 0
+            else:
+                grid.grid_array[clicked_pos_x][clicked_pos_y] = 1
+            
+    grid.conway(off_color=WHITE, on_color=BLUE, surface=screen, paused=pause)
+    pygame.display.update()
+
+pygame.quit()
